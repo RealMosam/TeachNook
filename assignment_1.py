3b36ac761b9d51ca506f59449f210ab778f1485f
Mosam Shambharkar <36242921+RealMosam@users.noreply.github.com>
2023-01-29 00:09:22 +0530

    Create assignment_1.py

---
+++ assignment_1.py
@@ -0,0 +1,16 @@
+# Assignment 1 
+# Create a two different examples for collective datatypes in python
+
+# LIST
+my_list = ['apple', 'mango', 'chikoo', 100, 200, 300]
+for i in my_list:
+  print(i)
+
+# DICTIONARY
+my_dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(my_dict)
